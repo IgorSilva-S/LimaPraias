2b98b39f95c082436f80494f31e4efc27c7b31ee
CAMILLY <cg3026311@labA204m37.IFSP>
2024-11-08 08:41:39 -0300

    atualizações login.css de 08/11

--- frontend/src/Components/Login/login.js
+++ frontend/src/Components/Login/login.js
@@ -2,23 +2,23 @@ import React from "react";
 import "./login.css";
 import logo from "../../assets/img/logo_lima.png";
 
-export default function Login() {
+export default function Login() { 
   return (
     <body id="mainlogin">
       <div id="containertexto">
           <img src={logo} alt="logo" id="logologin"/>
-          <h1 className="h1login" id="bemvindo">Bem Vindo!</h1>
-          <h3>FAVOR FAÇA LOGIN PARA ACESSAR A ÁREA DO FUNCIONÁRIO.</h3>
+          <h1 id="bemvindo">Bem Vindo!</h1>
+          <div id="mensagemlogin"><h3>Favor faça login para acessar a área do funcionário.</h3></div>
       </div>
         <div id="login">
           <div id="contentid">
-            <h1 className="h1login">LOGIN</h1>
+            <h1 id="h1login">LOGIN</h1>
             <form action="" method="get" class="form-login">
               <div class="form-login">
-                <input type="text" name="name" id="name" required />
+                <input type="text" name="name" id="name" placeholder="Nome:" required />
               </div>
               <div class="form-login">
-                <input type="email" name="email" id="email" required />
+                <input type="email" name="email" id="email" placeholder="E-mail:" required />
               </div>
               <div class="form-login">
                 <input type="submit" value="Subscribe!" />
