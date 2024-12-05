c1be62986bfe2338db8b504d7851f6209c96ce87
MrMystin <francisco.rocha@aluno.ifsp.edu.br>
2024-12-05 17:17:01 -0300

    FETCH in delete  page

--- frontend/src/Components/Delete/delete.js
+++ frontend/src/Components/Delete/delete.js
@@ -4,6 +4,40 @@ import SideBar from '../SideBar';
 import search from '../../assets/img/search-imgdel.png';
 
 const Delete = () => {
+  let cpf;
+  async function searchUser(event) {
+    event.preventDefault();
+    cpf = document.getElementById('cpf').value;
+    if (!cpf) {
+      alert('Digite um CPF!')
+    }
+    const response = await fetch(`http://localhost:3001/api/users/listUser/${cpf.replace(/[^\d]/g, "")}`, { method: "GET" });
+    const data = await response.json();
+    if (data.Erro) {
+      document.getElementById("cpf").value = null;
+      document.getElementById('nameOut').innerText = `Nome:`;
+      document.getElementById('surNameOut').innerText = `Sobrenome:`;
+      return alert("CPF não encontrado");
+    }
+    document.getElementById('nameOut').innerText = `Nome: ${data.user.fullName.split(' ').shift()}`;
+    document.getElementById('surNameOut').innerText = `Sobrenome: ${data.user.fullName.split(' ').pop()}`;
+  }
+
+  async function deleteUser(event) {
+    event.preventDefault();
+    if (!cpf) {
+      return alert("Digite um cpf!");
+    }
+    const response = await fetch(`http://localhost:3001/api/users/deleteUser/${cpf.replace(/[^\d]/g, "")}`, { method: "DELETE" });
+    if (response.status === 200) {
+      document.getElementById("cpf").value = null;
+      document.getElementById("surNameOut").innerText = `Sobrenome:`;
+      document.getElementById("surNameOut").innerText = `Sobrenome:`;
+      return alert("Usuário deletado com sucesso");
+    } else {
+      alert('Ocorreu um erro inesperado. Tente novamente mais tarde!')
+    }
+  }
   return (
     <>
       <main id="mainDelete">
@@ -15,16 +49,16 @@ const Delete = () => {
           <h2>CPF:</h2>
           <div className="searchInp-del">
             <input type="text" name="cpf" id="cpf" required />
-            <button id="lupa" className="searchButton-del">
+            <button onClick={searchUser} id="lupa" className="searchButton-del">
               <img src={search} alt="searchButton" />
             </button>
           </div>
         </div>
         <div class="info-del">
-          <p>Nome:</p>
-          <p>Sobrenome:</p>
+          <p id="nameOut">Nome:</p>
+          <p id="surNameOut">Sobrenome:</p>
         </div>
-        <button className="botaoDeletar">DELETAR</button>
+        <button onClick={deleteUser} className="botaoDeletar">DELETAR</button>
       </main>
     </>
   );
